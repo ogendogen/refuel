ab0b9a0ed26fe09b6ed592cf2b2e40615088639d
ogendogen <kosmos500@interia.pl>
2020-07-12 18:59:37 +0200

    Created models

---
+++ Database/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string Email { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ Database/Models/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public int Horsepower { get; set; }
+        public string Description { get; set; }
+        public uint OwnerID { get; set; }
+        public virtual ICollection<Refuel> Refuels { get; set; }
+    }
+}
