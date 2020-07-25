17ea2acb7cd9f37eb6edfa2dfea8247b4f0148e0
ogendogen <kosmos500@interia.pl>
2020-07-25 22:18:09 +0200

    Creating login and access denied pages

---
+++ Refuel/InputModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Refuel
+{
+    public class InputModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        [StringLength(20, ErrorMessage = "Login może mieć maksymalnie 20 znaków")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętać?")]
+        public bool Remember { get; set; }
+    }
+}
---
+++ Refuel/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Refuel.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public async Task OnGetAsync(string returnUrl = "")
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            returnUrl ??= Url.Content("~/");
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = "")
+        {
+            ReturnUrl = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                // authenticate
+            }
+
+            return Page();
+        }
+    }
+}
