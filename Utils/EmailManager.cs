356c769a0e5f48a81ad336016da1beee9c614af0
ogendogen <kosmos500@interia.pl>
2020-08-04 23:47:42 +0200

    Created "Utils" project with email manager

---
+++ Utils/EmailManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Database.Models;
+
+namespace Utils
+{
+    public class EmailManager : IEmailManager
+    {
+        public Task<bool> SendVerificationEmail(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Utils/IEmailManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Database.Models;
+
+namespace Utils
+{
+    public interface IEmailManager
+    {
+        Task<bool> SendVerificationEmail(User user);
+    }
+}
