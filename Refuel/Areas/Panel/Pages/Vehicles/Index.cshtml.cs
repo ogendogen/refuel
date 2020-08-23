a97446e47bd071933c1bc4fd21f8a5eccf42a20e
ogendogen <kosmos500@interia.pl>
2020-08-23 19:16:25 +0200

    Breadcrumbs done

--- Refuel/Areas/Panel/Pages/Vehicles/Index.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Vehicles/Index.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Refuel.Areas.Panel.Pages
+namespace Refuel.Areas.Panel.Pages.Vehicles
 {
-    public class VehiclesModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
