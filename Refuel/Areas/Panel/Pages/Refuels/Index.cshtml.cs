db0f8a5343e935cf604b71b7da5629dbe7d57d00
ogendogen <kosmos500@interia.pl>
2020-08-24 21:38:31 +0200

    Left side bar links fixed

--- Refuel/Areas/Panel/Pages/Refuels/Index.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Refuels/Index.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Refuel.Areas.Panel.Pages
+namespace Refuel.Areas.Panel.Pages.Refuels
 {
-    public class RefuelsModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
