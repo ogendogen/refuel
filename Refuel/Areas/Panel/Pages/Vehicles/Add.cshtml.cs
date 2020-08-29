25f6fbaf121bb9087280dbbc0838cc42f3f16c3a
ogendogen <kosmos500@interia.pl>
2020-08-30 00:21:26 +0200

    Built first version of add form

--- Refuel/Areas/Panel/Pages/Vehicles/Add.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Vehicles/Add.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refuel.Models;
 
 namespace Refuel.Areas.Panel.Pages.Vehicles
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public InputVehicleModel Input { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Refuel/Models/InputVehicleModel.cs
+++ Refuel/Models/InputVehicleModel.cs
@@ -9,20 +9,21 @@ namespace Refuel.Models
     public class InputVehicleModel
     {
         [Required]
-        [StringLength(32, ErrorMessage = "Nazwa firmy może mieć maksymalnie 32 znaki!")]
-        [Display(Name = "Firma")]
+        [StringLength(32, ErrorMessage = "Nazwa marki może mieć maksymalnie 32 znaki!")]
+        [Display(Name = "Marka")]
         public string Manufacturer { get; set; }
         [Required]
         [StringLength(32, ErrorMessage = "Nazwa modelu może mieć maksymalnie 32 znaki!")]
         [Display(Name = "Model")]
         public string Model { get; set; }
         [Required]
+        [Range(0.1, 100.0)]
         [Display(Name = "Silnik")]
         public decimal Engine { get; set; }
         [Required]
+        [Range(1, 1000)]
         [Display(Name = "Konie mechaniczne")]
         public int HorsePower { get; set; }
-        [Required]
         [DataType(DataType.MultilineText)]
         [Display(Name = "Opis")]
         public string Description { get; set; }
