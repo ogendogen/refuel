62a2db589f47c8ef72180cd821cbc9bff8609dc9
ogendogen <kosmos500@interia.pl>
2020-09-04 21:26:02 +0200

    Delete vehicle view

---
+++ Refuel/wwwroot/js/deleteCounter.js
@@ -0,0 +1,22 @@
+﻿var seconds = 10;
+
+$(document).ready(function () {
+
+    setTimeout(deleteButtonHandler, 1000);
+
+});
+
+function deleteButtonHandler()
+{
+    seconds--;
+    if (seconds > 0)
+    {
+        $("#deleteButton").html("Usuń (" + seconds + ")");
+        setTimeout(deleteButtonHandler, 1000);
+        return;
+    }
+
+    $("#deleteButton").html("Usuń");
+    $("#deleteButton").removeAttr("disabled");
+    $("#deleteButton").removeClass("cursor-notallowed");
+}
