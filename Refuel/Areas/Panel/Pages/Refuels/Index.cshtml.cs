e18da7cef4c2592a3e1e8b416b3d29f4481bf747
ogendogen <kosmos500@interia.pl>
2020-09-05 19:54:58 +0200

    Choosing vehicle from list

--- Refuel/Areas/Panel/Pages/Refuels/Index.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Refuels/Index.cshtml.cs
@@ -7,24 +7,27 @@ using Database;
 using Database.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refuel.Models;
 
 namespace Refuel.Areas.Panel.Pages.Refuels
 {
     public class IndexModel : PageModel
     {
         [BindProperty]
-        public IEnumerable<Vehicle> UserVehicles { get; set; }
+        public VehiclesListModel VehicleListModel { get; set; }
+        public List<Vehicle> UserVehicles { get; set; }
         private readonly IVehiclesManager _vehiclesManager;
 
         public IndexModel(IVehiclesManager vehiclesManager)
         {
             _vehiclesManager = vehiclesManager;
+            UserVehicles = new List<Vehicle>();
         }
 
         public void OnGet()
         {
             int userId = Int32.Parse(HttpContext.User.Claims.First(claim => claim.Type.Contains("nameidentifier")).Value);
-            UserVehicles = _vehiclesManager.GetAllUserVehicles(userId);
+            UserVehicles = _vehiclesManager.GetAllUserVehicles(userId).ToList();
         }
     }
 }
