f8ab927425ef09e13d7d57674b2e99d5ba015b4b
ogendogen <kosmos500@interia.pl>
2020-09-06 16:31:05 +0200

    Loading vehicle refuels

--- Refuel/Areas/Panel/Pages/Refuels/List.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Refuels/List.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Database.Models;
+using RefuelType = Database.Models.Refuel;
+using Database;
 
 namespace Refuel.Areas.Panel.Pages.Refuels
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<RefuelType> Refuels { get; set; }
+
+        private readonly IVehiclesManager _vehiclesManager;
+
+        public ListModel(IVehiclesManager vehiclesManager)
         {
+            _vehiclesManager = vehiclesManager;
+        }
+        public void OnGet(int vehicleId)
+        {
+            Vehicle vehicle = _vehiclesManager.GetVehicleById(vehicleId);
+            Refuels = _vehiclesManager.GetAllVehicleRefuelsSortedDescendingByDate(vehicle).ToList();
         }
     }
 }
