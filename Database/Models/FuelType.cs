1d36b19e72943a8b5f3ff011523c722d72cac5b1
ogendogen <kosmos500@interia.pl>
2020-09-06 16:49:45 +0200

    Format date and enum

--- Database/Models/FuelType.cs
+++ Database/Models/FuelType.cs
@@ -1,11 +1,18 @@
-﻿namespace Database.Models
+﻿using System.ComponentModel;
+
+namespace Database.Models
 {
     public enum FuelType
     {
+        [Description("Benzyna 95")]
         Petrol95,
+        [Description("Benzyna 98")]
         Petrol98,
+        [Description("Diesel")]
         Diesel,
+        [Description("GazLPG")]
         LPG,
+        [Description("Inne")]
         Other
     }
 }
---
+++ Utils/EnumDescriptionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Utils
+{
+    public static class EnumDescriptionHelper
+    {
+        public static string GetCustomDescription(object objEnum)
+        {
+            var fi = objEnum.GetType().GetField(objEnum.ToString());
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return (attributes.Length > 0) ? attributes[0].Description : objEnum.ToString();
+        }
+
+        public static string Description(this Enum value)
+        {
+            return GetCustomDescription(value);
+        }
+    }
+}
