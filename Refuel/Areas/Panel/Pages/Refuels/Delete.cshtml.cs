b2c7280c0830c09335050e2762bbedc0f0f8dfea
ogendogen <kosmos500@interia.pl>
2020-09-16 19:27:34 +0200

    Delete refuel added

--- Refuel/Areas/Panel/Pages/Refuels/Delete.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Refuels/Delete.cshtml.cs
@@ -2,15 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
+using Database.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RefuelType = Database.Models.Refuel;
 
 namespace Refuel.Areas.Panel.Pages.Refuels
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRefuelsManager _refuelsManager;
+
+        public int RefuelID { get; set; }
+
+        public DeleteModel(IRefuelsManager refuelsManager)
         {
+            _refuelsManager = refuelsManager;
+        }
+
+        public void OnGet(int refuelId)
+        {
+            RefuelID = refuelId;
+        }
+
+        public async Task<IActionResult> OnPostAsync(int refuelId)
+        {
+            int userId = Int32.Parse(HttpContext.User.Claims.First(claim => claim.Type.Contains("nameidentifier")).Value);
+            if (!_refuelsManager.IsUserOwnsRefuel(userId, refuelId))
+            {
+                return Forbid();
+            }
+
+            RefuelType refuel = _refuelsManager.GetRefuelById(refuelId);
+            int vehicleIdVar = refuel.Vehicle.ID;
+
+            await _refuelsManager.Delete(refuel);
+            TempData["status"] = "deleted";
+            return RedirectToPage("List", new {vehicleId = vehicleIdVar});
         }
     }
 }
