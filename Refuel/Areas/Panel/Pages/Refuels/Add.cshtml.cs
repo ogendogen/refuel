e0b5945468292353266fc99311f99afce29f6048
ogendogen <kosmos500@interia.pl>
2020-09-17 15:18:23 +0200

    Added empty "add refuel" page

--- Refuel/Areas/Panel/Pages/Refuels/Add.cshtml.cs
+++ Refuel/Areas/Panel/Pages/Refuels/Add.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refuel.Models;
 
 namespace Refuel.Areas.Panel.Pages.Refuels
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public InputRefuelModel Input { get; set; }
         public void OnGet()
         {
         }
