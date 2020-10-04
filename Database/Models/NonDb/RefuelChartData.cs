95374ee1dce746cc9c50fb7994030a363ee002d7
ogendogen <kosmos500@interia.pl>
2020-10-04 19:53:28 +0200

    Added RefuelChartData model

---
+++ Database/Models/NonDb/RefuelChartData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models.NonDb
+{
+    public class RefuelChartData
+    {
+        public DateTime RefuelDate { get; set; }
+        public decimal Price { get; set; }
+        public decimal Combustion { get; set; }
+    }
+}
