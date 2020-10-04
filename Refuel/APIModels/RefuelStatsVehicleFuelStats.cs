f8c61540a0cd906db72d2b67870b5e8242025a62
ogendogen <kosmos500@interia.pl>
2020-10-04 19:55:13 +0200

    Added RefuelStatsVehicleFuelStats model

---
+++ Refuel/APIModels/RefuelStatsVehicleFuelStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models.NonDb;
+
+namespace Refuel.APIModels
+{
+    public class RefuelStatsVehicleFuelStats : RefuelStatsAPIResponse
+    {
+        public List<RefuelChartData> RefuelsDataForCharts { get; set; }
+        public decimal FuelTotalPrice { get; set; }
+    }
+}
