f05da3a9e0710f832a26771de532c96bc3ae2c46
ogendogen <kosmos500@interia.pl>
2020-10-17 19:32:54 +0200

    Removed usage of "Description" attribute

--- Database/Models/FuelType.cs
+++ Database/Models/FuelType.cs
@@ -5,20 +5,15 @@ namespace Database.Models
 {
     public enum FuelType
     {
-        [Description("Benzyna 95")]
         [Display(Name="Benzyna 95")]
         Petrol95,
         [Display(Name="Benzyna 98")]
-        [Description("Benzyna 98")]
         Petrol98,
         [Display(Name="Diesel")]
-        [Description("Diesel")]
         Diesel,
         [Display(Name="GazLPG")]
-        [Description("GazLPG")]
         LPG,
         [Display(Name="Inne")]
-        [Description("Inne")]
         Other
     }
 }
