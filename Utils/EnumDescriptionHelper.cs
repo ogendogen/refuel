9a1d74dce50992346820a857b9e51ba09bfe38b2
ogendogen <kosmos500@interia.pl>
2020-10-18 01:54:33 +0200

    Table with each fuel costs

--- Utils/EnumDescriptionHelper.cs
+++ Utils/EnumDescriptionHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -13,6 +14,18 @@ namespace Utils
             var fi = objEnum.GetType().GetField(objEnum.ToString());
             var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
             return (attributes.Length > 0) ? attributes[0].Description : objEnum.ToString();
+        }
+
+        public static string GetDisplayName(object objEnum)
+        {
+            var fi = objEnum.GetType().GetField(objEnum.ToString());
+            var displayName = fi.GetCustomAttributesData().FirstOrDefault()?.NamedArguments?.FirstOrDefault().TypedValue.ToString().Trim('"');
+            return displayName ?? objEnum.ToString();
+        }
+
+        public static string DisplayName(this Enum value)
+        {
+            return GetDisplayName(value);
         }
 
         public static string Description(this Enum value)
